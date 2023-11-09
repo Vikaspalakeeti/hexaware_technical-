2cb6028a2b0bd11e6e0406d09310a3ce03809a6e
Vikaspalakiti <76863142+Vikaspalakiti@users.noreply.github.com>
2023-11-09 22:07:18 +0530

    Add files via upload
    vikas updated after karthik

---
+++ OnlineFoodDeliveryApp/OnlineFoodDeliveryApp/src/main/java/com/hexaware/fooddelivery/entity/Cart.java
@@ -0,0 +1,108 @@
+package com.hexaware.fooddelivery.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Cart {
+	@Id
+	private int cart_id;
+	private int customer_id;
+	private int restaurant_id;
+	private int item_id;
+	private double price;
+	private int quantity;
+	private double total;
+	private int order_id;
+	
+	public Cart() {
+		super();
+	}
+
+	public Cart(int cart_id, int customer_id, int restaurant_id, int item_id, double price, int quantity, double total,
+			int order_id) {
+		super();
+		this.cart_id = cart_id;
+		this.customer_id = customer_id;
+		this.restaurant_id = restaurant_id;
+		this.item_id = item_id;
+		this.price = price;
+		this.quantity = quantity;
+		this.total = total;
+		this.order_id = order_id;
+	}
+
+	public int getCart_id() {
+		return cart_id;
+	}
+
+	public void setCart_id(int cart_id) {
+		this.cart_id = cart_id;
+	}
+
+	public int getCustomer_id() {
+		return customer_id;
+	}
+
+	public void setCustomer_id(int customer_id) {
+		this.customer_id = customer_id;
+	}
+
+	public int getRestaurant_id() {
+		return restaurant_id;
+	}
+
+	public void setRestaurant_id(int restaurant_id) {
+		this.restaurant_id = restaurant_id;
+	}
+
+	public int getItem_id() {
+		return item_id;
+	}
+
+	public void setItem_id(int item_id) {
+		this.item_id = item_id;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public double getTotal() {
+		return total;
+	}
+
+	public void setTotal(double total) {
+		this.total = total;
+	}
+
+	public int getOrder_id() {
+		return order_id;
+	}
+
+	public void setOrder_id(int order_id) {
+		this.order_id = order_id;
+	}
+
+	@Override
+	public String toString() {
+		return "Cart [cart_id=" + cart_id + ", customer_id=" + customer_id + ", restaurant_id=" + restaurant_id
+				+ ", item_id=" + item_id + ", price=" + price + ", quantity=" + quantity + ", total=" + total
+				+ ", order_id=" + order_id + "]";
+	}
+	
+	
+	
+}
---
+++ OnlineFoodDeliveryApp/OnlineFoodDeliveryApp/src/main/java/com/hexaware/fooddelivery/entity/Menu.java
@@ -0,0 +1,66 @@
+package com.hexaware.fooddelivery.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Menu {
+	@Id
+	private int menu_item_id;
+	private int restaurant_id;
+	private String item_name;
+	private String description;
+	private double price;
+	private boolean availability;
+	public Menu() {
+		super();
+	}
+	public Menu(int menu_item_id, int restaurant_id, String item_name, String description, double price,
+			boolean availability) {
+		super();
+		this.menu_item_id = menu_item_id;
+		this.restaurant_id = restaurant_id;
+		this.item_name = item_name;
+		this.description = description;
+		this.price = price;
+		this.availability = availability;
+	}
+	public int getMenu_item_id() {
+		return menu_item_id;
+	}
+	public void setMenu_item_id(int menu_item_id) {
+		this.menu_item_id = menu_item_id;
+	}
+	public int getRestaurant_id() {
+		return restaurant_id;
+	}
+	public void setRestaurant_id(int restaurant_id) {
+		this.restaurant_id = restaurant_id;
+	}
+	public String getItem_name() {
+		return item_name;
+	}
+	public void setItem_name(String item_name) {
+		this.item_name = item_name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public boolean isAvailability() {
+		return availability;
+	}
+	public void setAvailability(boolean availability) {
+		this.availability = availability;
+	}
+	
+	
+}
