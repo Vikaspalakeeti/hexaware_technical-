e45fc6898c1f5247e44042c2b81da5b54563c53a
karthik <76810559+9948954894karthik9398444491@users.noreply.github.com>
2023-11-12 14:33:10 +0530

    Delete OnlineFoodDeliveryApp directory

--- OnlineFoodDeliveryApp/src/main/java/com/hexaware/fooddelivery/repository/MenuRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hexaware.fooddelivery.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.hexaware.fooddelivery.entity.Menu;
-
-public interface MenuRepository extends JpaRepository<Menu, Integer>{
-	Menu findByItemName(String itemName);
-}
