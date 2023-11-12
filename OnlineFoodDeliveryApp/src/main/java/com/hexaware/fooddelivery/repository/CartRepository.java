d191779daeeeec93f290733064d84397de05388f
karthik <76810559+9948954894karthik9398444491@users.noreply.github.com>
2023-11-12 22:28:34 +0530

    Delete OnlineFoodDeliveryApp directory

--- OnlineFoodDeliveryApp/src/main/java/com/hexaware/fooddelivery/repository/CartRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hexaware.fooddelivery.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.hexaware.fooddelivery.entity.Cart;
-
-public interface CartRepository extends JpaRepository<Cart, Integer>{
-	
-}
